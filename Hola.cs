02221b85503c39b2d62e67e72b59b94e8b8216b4
zDRKLL <152322820+zDRKLL@users.noreply.github.com>
2023-11-28 19:43:31 +0100

    Add files via upload

---
+++ Hola.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class Hola : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("HelloWorld");
+        print("1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
+
+
+    
